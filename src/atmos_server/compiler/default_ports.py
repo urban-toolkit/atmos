5217ca98213e31465d809912f3e309a55024d1e4
Carolina Veiga <carolvfs@gmail.com>
2026-03-06 09:13:39 -0500

    starting refactoring

--- src/atmos_server/compiler/default_ports.py
+++ src/atmos_server/compiler/default_ports.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from atmos_server.compiler.adapters.repo_root import PackageRepoRootResolver
-from atmos_server.compiler.adapters.render_maplibre import MapLibreRenderBuilder
-from atmos_server.compiler.adapters.xarray_time import XarrayTimeLenResolver
+from atmos_server.adapters.repo_root import PackageRepoRootResolver
+from atmos_server.adapters.render_maplibre import MapLibreRenderBuilder
+from atmos_server.adapters.xarray_time import XarrayTimeLenResolver
 from atmos_server.compiler.ports import CompilerPorts
 
 
